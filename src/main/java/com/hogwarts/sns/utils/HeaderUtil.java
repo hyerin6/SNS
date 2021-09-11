282fcb347920ccab04875128b1a292c071b90469
hyerin6 <hyerinn6@gmail.com>
2021-09-12 01:06:08 +0900

    refactor: HeaderUtil

--- src/main/java/com/hogwarts/sns/utils/HeaderUtil.java
+++ src/main/java/com/hogwarts/sns/utils/HeaderUtil.java
@@ -2,36 +2,24 @@ package com.hogwarts.sns.utils;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.WebRequest;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class HeaderUtil {
-
 	private static final String HEADER_AUTH = "Authorization";
 
 	private static final String BEARER_PREFIX = "Bearer ";
 
 	public static String getAccessToken(HttpServletRequest request) {
 		String token = request.getHeader(HEADER_AUTH);
-
-		if (StringUtils.hasText(token) && token.startsWith(BEARER_PREFIX)) {
-			return token.substring(7);
-		}
-
-		return null;
+		return token.substring(BEARER_PREFIX.length());
 	}
 
 	public static String getAccessToken(WebRequest request) {
 		String token = request.getHeader(HEADER_AUTH);
-
-		if (StringUtils.hasText(token) && token.startsWith(BEARER_PREFIX)) {
-			return token.substring(7);
-		}
-
-		return null;
+		return token.substring(BEARER_PREFIX.length());
 	}
 
 }
