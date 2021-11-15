33903b96e661f490b721661a12b930bb95235bbf
hyerin6 <hyerinn6@gmail.com>
2021-11-15 13:59:22 +0900

    test

--- src/main/java/com/hogwarts/sns/infrastructure/configuration/ElasticSearchConfig.java
+++ src/main/java/com/hogwarts/sns/infrastructure/configuration/ElasticSearchConfig.java
@@ -6,18 +6,19 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @EnableElasticsearchRepositories("com.hogwarts.sns.infrastructure.persistence")
 @Configuration
-public class ElasticSearchConfig {
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
 	@Value("${spring.elasticsearch.rest.uris}")
 	String elasticHost;
 
+	@Override
 	@Bean
-	public RestHighLevelClient restHighLevelClient() {
+	public RestHighLevelClient elasticsearchClient() {
 		return new RestHighLevelClient(RestClient.builder(HttpHost.create(elasticHost)));
 	}
-
 }
